595ac608a2afc379da37f4308a1483e416b47f42
Logan <redkal19@gmail.com>
2022-09-02 19:45:02 +0900

    v0.1.1.1 페이징 처리 & 등록 기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.study.board.controller;
 
+import com.study.board.dto.BoardDTO;
 import com.study.board.dto.PageRequestDTO;
 import com.study.board.model.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -28,5 +31,24 @@ public class BoardController {
 
         log.info("List...................." + pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String resiterPost(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+        //addFlashAttribute : 한번만 데이터 전달하는 용도
+        //msg : 등록 시, 확인 모달 창
+
+
+        return "redirect:/board/list";
     }
 }
